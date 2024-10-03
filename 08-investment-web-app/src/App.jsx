b7ab6f4f27e4804509fbef3d22280493f2d30967
Tresten Pool <trestenp@cctexas.com>
2024-10-03 17:00:51 -0500

    investment starting code added

--- 08-investment-web-app/src/App.jsx
+++ 08-investment-web-app/src/App.jsx
@@ -1,11 +1,27 @@
-import ResultsTable from "./ResultsTable"
-import UserInput from "./UserInput"
-import { useState } from "react"
+import ResultsTable from "./ResultsTable";
+import UserInput from "./UserInput";
+import { useState } from "react";
 
 function App() {
+  // our state
   const [investmentInput, setInvestmentInput] = useState({
-    year
+    initialInvestment: 1000,
+    annualInvestment: 500,
+    expectedReturn: 5.5,
+    duration: 12,
   });
+
+  /*******************************************/
+  /************* Helper functions ************/
+  /*******************************************/
+  function handleUserInputChange(event, userInputType) {
+    console.log(typeof userInputType);
+    console.dir(investmentInput);
+
+    setInvestmentInput((prevState) => {
+      return { ...prevState, userInputType: event.target.value };
+    });
+  }
 
   /*******************************************/
   /*************** JSX CODE ******************/
@@ -16,12 +32,13 @@ function App() {
       <h1 id="header">React Investment Calculator</h1>
 
       {/* User input */}
-      <UserInput />
+      <UserInput onChangeFunction={handleUserInputChange} />
 
       {/* Table of results */}
       <ResultsTable />
+      {investmentInput.initialInvestment}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- 08-investment-web-app/src/UserInput.jsx
+++ 08-investment-web-app/src/UserInput.jsx
@@ -1,23 +1,33 @@
-export default function UserInput({...props}) {
+export default function UserInput({ onChangeFunction, ...props }) {
   return (
     <div id="user-input">
       <div className="input-group">
         <label>
-          Initial Investment<input type="number" id="initialInvestment" name="userForm" placeholder="1000"/>
+          Initial Investment
+          <input
+            onChange={(event) => onChangeFunction(event, "initialInvestment")}
+            type="number"
+            id="initialInvestment"
+            name="userForm"
+            placeholder="1000"
+          />
         </label>
 
         <label>
-          Annual Investment<input type="number" id="annualInvestment" name="userForm"/>
+          Annual Investment
+          <input type="number" id="annualInvestment" name="userForm" />
         </label>
 
         <label>
-          Expected Return<input type="number" id="expectedReturn" name="userForm"/>
+          Expected Return
+          <input type="number" id="expectedReturn" name="userForm" />
         </label>
 
         <label>
-          Duration<input type="number" id="duration" name="userForm"/>
+          Duration
+          <input type="number" id="duration" name="userForm" />
         </label>
       </div>
     </div>
-  )
+  );
 }
