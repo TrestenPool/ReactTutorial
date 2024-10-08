ec405a71c92e1be862aa3f22833c1e5cdd92fb03
Tresten Pool <trestenp@cctexas.com>
2024-10-08 16:45:42 -0500

    Added to 09 tailwind portion

--- 09-Applying-CSS-Styles-Tailwind/src/components/AuthInputs.jsx
+++ 09-Applying-CSS-Styles-Tailwind/src/components/AuthInputs.jsx
@@ -4,25 +4,6 @@ import Button from "./Button.jsx";
 import Input from "./Input.jsx";
 
 /*****************************/
-/**** STYLED COMPONENTS ******/
-/*****************************/
-const ControlContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`;
-
-const TextButton = styled.button`
-  color: #f0b322;
-  border: none;
-
-  &:hover {
-    color: #f0920e;
-  }
-`;
-
-/*****************************/
 /******** COMPONENT **********/
 /*****************************/
 export default function AuthInputs() {
@@ -59,31 +40,27 @@ export default function AuthInputs() {
   return (
     <div id="auth-inputs">
       {/* Contains the inputs */}
-      <ControlContainer>
+      <div>
         <p>
-          <Input
+          <input
             type="Email"
-            $invalid={emailNotValid}
-            label="Email"
             onChange={(event) => handleInputChange("email", event.target.value)}
           />
         </p>
         <p>
-          <Input
+          <input
             type="password"
-            label="Password"
-            $invalid={passwordNotValid}
             onChange={(event) =>
               handleInputChange("password", event.target.value)
             }
           />
         </p>
-      </ControlContainer>
+      </div>
 
       {/* The buttons section on the bottom */}
       <div className="actions">
-        <TextButton onClick={handleLogin}>Create an account</TextButton>
-        <Button onClick={handleLogin}>Sign In</Button>
+        <button onClick={handleLogin}>Create an account</button>
+        <button onClick={handleLogin}>Sign In</button>
       </div>
     </div>
   );
