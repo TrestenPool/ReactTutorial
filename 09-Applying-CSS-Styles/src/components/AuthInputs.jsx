a4eca08a2bf1af3e13c268fef7dd4608826b3f43
Tresten Pool <trestenp@cctexas.com>
2024-10-08 08:07:28 -0500

    Finished 09

--- 09-Applying-CSS-Styles/src/components/AuthInputs.jsx
+++ 09-Applying-CSS-Styles/src/components/AuthInputs.jsx
@@ -1,18 +1,34 @@
-import { useState } from 'react';
+import { useState } from "react";
+import { styled } from "styled-components";
+import Button from "./Button.jsx";
+import Input from "./Input.jsx";
 
+/*****************************/
+/**** STYLED COMPONENTS ******/
+/*****************************/
+const ControlContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  margin-bottom: 1.5rem;
+`;
+
+/*****************************/
+/******** COMPONENT **********/
+/*****************************/
 export default function AuthInputs() {
   /*****************/
   /**** STATE ******/
   /*****************/
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [enteredPassword, setEnteredPassword] = useState('');
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredPassword, setEnteredPassword] = useState("");
   const [submitted, setSubmitted] = useState(false);
 
   /*****************/
   /*** FUNCTIONS ***/
   /*****************/
   function handleInputChange(identifier, value) {
-    if (identifier === 'email') {
+    if (identifier === "email") {
       setEnteredEmail(value);
     } else {
       setEnteredPassword(value);
@@ -25,7 +41,7 @@ export default function AuthInputs() {
   /***********************/
   /*** COMPUTE VALUES ***/
   /***********************/
-  const emailNotValid = submitted && !enteredEmail.includes('@');
+  const emailNotValid = submitted && !enteredEmail.includes("@");
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   /***********************/
@@ -33,31 +49,34 @@ export default function AuthInputs() {
   /***********************/
   return (
     <div id="auth-inputs">
-      <div className="controls">
+      {/* Contains the inputs */}
+      <ControlContainer>
         <p>
-          <label>Email</label>
-          <input
-            type="email"
-            className={emailNotValid ? 'invalid' : undefined}
-            onChange={(event) => handleInputChange('email', event.target.value)}
+          <Input
+            type="Email"
+            $invalid={emailNotValid}
+            label="Email"
+            onChange={(event) => handleInputChange("email", event.target.value)}
           />
         </p>
         <p>
-          <label>Password</label>
-          <input
+          <Input
             type="password"
-            className={passwordNotValid ? 'invalid' : undefined}
+            label="Password"
+            $invalid={passwordNotValid}
             onChange={(event) =>
-              handleInputChange('password', event.target.value)
+              handleInputChange("password", event.target.value)
             }
           />
         </p>
-      </div>
+      </ControlContainer>
+
+      {/* The buttons section on the bottom */}
       <div className="actions">
-        <button type="button" className="text-button">
-          Create a new account
-        </button>
-        <button className='button' onClick={handleLogin}>Sign In</button>
+        <Button className="text-button" onClick={handleLogin}>
+          Something else
+        </Button>
+        <Button onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
   );
