61b1ba034a9af7b1fca43d3ffb4ef60ed3650b0e
Tresten Pool <trestenpool@gmail.com>
2024-10-14 07:15:46 -0500

    Added 11

--- 11-Refs-Portals-Intro/src/components/Player.jsx
+++ 11-Refs-Portals-Intro/src/components/Player.jsx
@@ -1,11 +1,31 @@
+import {useState, useRef} from 'react';
+
 export default function Player() {
+  // REF
+  const playerName = useRef();  
+
+  // STATE
+  const [enteredPlayerName, setEnteredPlayerName] = useState();
+
+  function handleClick(){
+    setEnteredPlayerName(playerName.current.value);
+    playerName.current.value = '';
+  }
+
+  // JSX Code
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+
+      {/* Title */}
+      <h2>Welcome {enteredPlayerName ?? 'Unknown Entity'}</h2>
+
+
+      {/* input field */}
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerName} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
+
     </section>
   );
 }
